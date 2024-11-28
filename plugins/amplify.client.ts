90872c3bf0cd85beec703f926956cc73f2d5dacd
sapotrotas <brunoqvra@gmail.com>
2024-11-28 22:51:12 +0000

    build again

--- plugins/amplify.client.ts
+++ plugins/amplify.client.ts
@@ -2,7 +2,13 @@ import { Amplify } from "aws-amplify"
 import { defineNuxtPlugin } from "#app"
 import outputs from "../amplify_outputs.json"
 
+//Amplify.configure(outputs)
+
 export default defineNuxtPlugin((NuxtApp) => {
 
-  Amplify.configure(outputs)
+  return {
+    provide: {
+      amplifyConfigs: Amplify.configure(outputs)
+    }
+  }
 })
